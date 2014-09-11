0b04ea4ff48d058dd3b124093189a7825747aeb4
John Berry <jberry+githubcommits@lumoslabs.com>
2014-09-10 21:59:00 -0700

    Add create and update methods

--- app/controllers/chores_controller.rb
+++ app/controllers/chores_controller.rb
@@ -10,4 +10,18 @@ class ChoresController < ApplicationController
     @chore = Chore.find(params[:id])
     render json: @chore
   end
+
+  def create
+    @chore = Chore.create!(params[:chore])
+    render json: @chore
+  end
+
+  def update
+    @chore = Chore.find(params[:id])
+    if @chore.update_attributes!(params[:chore])
+      render json: @chore
+    else
+      render nothing: true, status: 400
+    end
+  end
 end
--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,3 +1,6 @@
 class Chore < ActiveRecord::Base
   attr_accessible :name, :summary, :est_minutes
+
+  validates_presence_of :name
+  validates_numericality_of :est_minutes
 end
